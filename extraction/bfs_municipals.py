74ad775193a6e6473f654769dab238bc2ebecb37
patywell <patricia.wellhoefer@gmx.ch>
2022-04-03 18:40:35 +0200

    reading "Gemeinde Kennzahlen" properly

---
+++ extraction/bfs_municipals.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+
+url = "https://dam-api.bfs.admin.ch/hub/api/dam/assets/7226419/master"
+r = requests.get(url)
+data = r.content
+df_zipcode = pd.read_excel(data, sheet_name="PLZ6",
+                           usecols=[0, 1, 2, 4, 5, 6],
+                           header=None,
+                           names=["zipcode_4", "zipcode_6",
+                                  "zipcode_name", "canton_abbrev",
+                                  "municip_nr", "municip_name"],
+                           skiprows=1)
+print(df_zipcode)
