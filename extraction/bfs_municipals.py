2dce910d19e6a6b56e8a0e96f03a655c1f729653
patywell <patricia.wellhoefer@gmx.ch>
2022-04-03 18:58:48 +0200

    extract data

--- extraction/bfs_municipals.py
+++ extraction/bfs_municipals.py
@@ -2,13 +2,15 @@ import pandas as pd
 import requests
 
 url = "https://dam-api.bfs.admin.ch/hub/api/dam/assets/7226419/master"
-r = requests.get(url)
-data = r.content
-df_zipcode = pd.read_excel(data, sheet_name="PLZ6",
+
+def create_df_zipcode(url):
+    r = requests.get(url)
+    data = r.content
+    df_zipcode = pd.read_excel(data, sheet_name="PLZ6",
                            usecols=[0, 1, 2, 4, 5, 6],
                            header=None,
                            names=["zipcode_4", "zipcode_6",
                                   "zipcode_name", "canton_abbrev",
                                   "municip_nr", "municip_name"],
                            skiprows=1)
-print(df_zipcode)
+    return df_zipcode
