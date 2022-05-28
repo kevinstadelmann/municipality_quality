92ce2fec9581896545b875a2af4d3271566ef9c0
ma-ch-we <webermarc87@gmail.com>
2022-05-29 00:56:51 +0200

    Update Oceanloader - Add detail log message for SQLalchemy errors marc weber 29.05.2022

--- load/load_to_db_dwh.py
+++ load/load_to_db_dwh.py
@@ -1,21 +1,25 @@
 import logging
-from sqlalchemy.sql import text
+from sqlalchemy import exc
 from municipality_quality.database_management import dbconnector as db_mgmt
 
-def upload_to_db_dwh(airflow_connection_db: str, job_id: int, proc_name: str, db_schema: str) -> None:
-
+def upload_to_db_dwh(airflow_connection_db: str, job_id: str, proc_name: str, db_schema: str):
     conn = db_mgmt.DBConnector(airflow_connection_db)
     engine = conn.connect_datalake()
     connection = engine.connect()
-    logging.info("Connection to db established")
+    logging.info('Connection to db established')
 
-    parameter1 = job_id
+    parameter1 = str(job_id)
     parameter2 = db_schema + "." + proc_name
+    statement = 'CALL ' + parameter2 + ' (' + parameter1 + ')'
 
     try:
-        connection.execute(text("CALL :p2 (:p1)"),p1=parameter1, p2=parameter2)
+        return_message = ['0', 'Load to dwh end']
+        connection.execute(statement)
         connection.execute("commit")
-    except:
-        logging.info('Could not execute dwh procedure. Please check db.')
+        connection.close()
+    except exc.SQLAlchemyError as err:
+        return_message = [99, f'Failed to execute {statement}: {str(err.orig)}']
     finally:
         connection.close()
+
+    return return_message
